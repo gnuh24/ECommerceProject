20d9be6ae96bb9282e17038ff7abb6c05b2ced60
THug24 <devilskipper@gmail.com>
2024-09-20 07:23:53 +0700

    Setup project & Configure Database MySQL

---
+++ EC_BackEnd/Configure/MysqlConfig.php
@@ -0,0 +1,30 @@
+<?php
+
+class MysqlConfig
+{
+
+  private static $servername = "localhost"; // Tên máy chủ
+  private static $username = "root"; // Tên người dùng MySQL
+  private static $password = "0204"; // Mật khẩu MySQL
+  private static $database = "SGU_E_Commerce"; // Tên cơ sở dữ liệu
+  private static $port = 3306;
+  public static function getConnection()
+  {
+
+    try {
+      //Tạo connection
+      $conn = new PDO(
+        "mysql:host=" . self::$servername . ";
+                                port=" . self::$port . ";
+                                dbname=" . self::$database,
+        self::$username,
+        self::$password,
+      );
+
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
+    } catch (PDOException $e) {
+      echo "Kết nối không thành công: " . $e->getMessage();
+    }
+  }
+}
